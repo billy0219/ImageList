0c8275732cac26008b28ad2d101d64e1c082255d
billy0219 <billy0219@gmail.com>
2017-07-29 20:19:55 +0900

    1st update

--- app/src/main/java/com/example/billy/imagelist/MainActivity.java
+++ app/src/main/java/com/example/billy/imagelist/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.billy.imagelist;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
@@ -22,8 +23,9 @@ public class MainActivity extends AppCompatActivity {
         MyRecyclerAdapter adapter = new MyRecyclerAdapter(this, createItemList(), R.layout.item_row);
         recyclerView.setAdapter(adapter);
 
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+//        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+//        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
         recyclerView.setLayoutManager(layoutManager);
 
         recyclerView.setItemAnimator(new DefaultItemAnimator());
@@ -33,6 +35,11 @@ public class MainActivity extends AppCompatActivity {
         List<ViewItem> items = new ArrayList<ViewItem>();
         for (int i = 0; i < 6; i++) {
             items.add(new ViewItem( R.drawable.a, "아이템"+i ));
+            items.add(new ViewItem( R.drawable.b, "아이템"+i ));
+            items.add(new ViewItem( R.drawable.c, "아이템"+i ));
+            items.add(new ViewItem( R.drawable.d, "아이템"+i ));
+            items.add(new ViewItem( R.drawable.e, "아이템"+i ));
+            items.add(new ViewItem( R.drawable.f, "아이템"+i ));
         }
         return items;
     }
